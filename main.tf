50f96e0df11d2e040c043c297ffe189d52901825
Richard Cheney <richeney@microsoft.com>
2021-05-03 16:28:27 +0100

    Consolidated to avoid subnet-nsg policy issue

--- main.tf
+++ main.tf
@@ -33,21 +33,18 @@ resource "azurerm_network_security_group" "pls" {
   name                = "networkSecurityGroup"
   location            = azurerm_resource_group.pls.location
   resource_group_name = azurerm_resource_group.pls.name
-}
 
-resource "azurerm_network_security_rule" "web" {
-  resource_group_name         = azurerm_resource_group.pls.name
-  network_security_group_name = azurerm_network_security_group.pls.name
-
-  name                                       = "nginx"
-  priority                                   = 1003
-  direction                                  = "Inbound"
-  access                                     = "Allow"
-  protocol                                   = "Tcp"
-  source_address_prefix                      = "*"
-  source_port_range                          = "*"
-  destination_application_security_group_ids = [azurerm_application_security_group.linux.id]
-  destination_port_ranges                    = ["80", "443"]
+  security_rule {
+    name                                       = "nginx"
+    priority                                   = 1003
+    direction                                  = "Inbound"
+    access                                     = "Allow"
+    protocol                                   = "Tcp"
+    source_address_prefix                      = "*"
+    source_port_range                          = "*"
+    destination_application_security_group_ids = [azurerm_application_security_group.linux.id]
+    destination_port_ranges                    = ["80", "443"]
+  }
 }
 
 resource "azurerm_virtual_network" "pls" {
@@ -55,32 +52,26 @@ resource "azurerm_virtual_network" "pls" {
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.pls.location
   resource_group_name = azurerm_resource_group.pls.name
-}
 
-resource "azurerm_subnet" "pls" {
-  name                 = "subnet"
-  resource_group_name  = azurerm_resource_group.pls.name
-  virtual_network_name = azurerm_virtual_network.pls.name
-  address_prefixes     = ["10.0.1.0/24"]
+  subnet {
+    name           = "saas"
+    address_prefix = "10.0.1.0/24"
+    security_group = azurerm_network_security_group.pls.id
+  }
 }
-
-resource "azurerm_subnet_network_security_group_association" "pls" {
-  subnet_id                 = azurerm_subnet.pls.id
-  network_security_group_id = azurerm_network_security_group.pls.id
-}
-
-// Linux virtual machines
 
 module "linux_vm" {
   source              = "./linux-vm"
   resource_group_name = azurerm_resource_group.pls.name
   location            = azurerm_resource_group.pls.location
+  depends_on          = [azurerm_virtual_network.pls]
 
   for_each = toset(["web1", "web2", "web3"])
 
   name                 = each.value
-  subnet_id            = azurerm_subnet.pls.id
+  subnet_id            = azurerm_virtual_network.pls.subnet.*.id[0] // No idea why this is the syntax
   asg_id               = azurerm_application_security_group.linux.id
   admin_ssh_public_key = azurerm_ssh_public_key.pls.public_key
   zone                 = trimprefix(each.value, "web")
+  ip_address           = cidrhost("10.0.1.0/24", trimprefix(each.value, "web") + 3)
 }
